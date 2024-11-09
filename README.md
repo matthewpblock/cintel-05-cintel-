6c910a7fa4c219a9676608dfd79ec889a450a163
matthewpblock <matthewpblock@gmail.com>
2024-11-08 17:48:50 -1000

    Action 2 Plan Our Reactive Content

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 Module 5 Project for Continuous Intelligence
 
 ## Built locally in repository MS in Data Analytics> cd cintel-05-cintel-
-## Downloading the faicons to my machine to enable local development
+## Downloaded the faicons to my machine to enable local development -- required 'pip install faicons'
--- dashboard/app.py
+++ dashboard/app.py
@@ -5,3 +5,15 @@ import random
 from datetime import datetime
 from faicons import icon_svg
 
+#set a constant UPDATE INTERVAL for all live data
+UPDATE_INTERVAL_SECS: int = 1
+
+# Initialize a REACTIVE CALC that our display components can call
+@reactive.calc()
+def reactive_calc_combined():
+        reactive.invalidate_later(UPDATE_INTERVAL_SECS) # Invalidates this calculation every UPDATE_INTERVAL_SECS to trigger updates
+        temp = round(random.uniform(-18, -16), 1) # Gets random number between -18 and -16 C, rounded to 1 decimal place
+        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S") # Gets a timestamp in the format "YYYY-MM-DD HH:MM:SS"
+        latest_dictionary_entry = {"temp": temp, "timestamp": timestamp}
+        
+        return latest_dictionary_entry #Displays the results
